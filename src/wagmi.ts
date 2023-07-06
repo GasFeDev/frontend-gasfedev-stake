f28f1f3ac866805f6435f681481dbcd68f036ea1
GasFeDev <gasfedev@gmail.com>
2023-07-06 19:33:56 -0300

    test  wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -4,7 +4,7 @@ import { configureChains, createClient } from 'wagmi';
 import { goerli } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
-interface CustomWindow extends Window {
+/* interface CustomWindow extends Window {
   env: {
     REACT_APP_ALCHEMY_API_KEY: string;
     REACT_APP_PROJECTID_WALLET_CONNECT: string;
@@ -15,9 +15,10 @@ declare const window: CustomWindow;
 
 
 const alchemyApiKey = window.env.REACT_APP_ALCHEMY_API_KEY!;
-const wallectConnectProjectId = window.env.REACT_APP_PROJECTID_WALLET_CONNECT;
+const wallectConnectProjectId = window.env.REACT_APP_PROJECTID_WALLET_CONNECT; */
 
-
+const alchemyApiKey = process.env.REACT_APP_ALCHEMY_API_KEY!;
+const wallectConnectProjectId = "d5f62bd5f563583be230f939b64c9e14"
 
 const { chains, provider, webSocketProvider } = configureChains(
   [goerli],
