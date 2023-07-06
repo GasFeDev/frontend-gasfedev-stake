7570f9aadad55a4d28f0021548c2f1a2cd2ac1e5
GasFeDev <gasfedev@gmail.com>
2023-07-06 18:52:00 -0300

    Envaironment variable in HTML

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,15 +2,12 @@ import { getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient } from 'wagmi';
 import { goerli } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 const alchemyApiKey = process.env.REACT_APP_ALCHEMY_API_KEY!;
-const wallectConnectProjectId = process.env.REACT_APP_PROJECTID_WALLET_CONNECT;
+const wallectConnectProjectId = process.env.REACT_APP_PROJECTID_WALLET_CONNECT!;
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [ goerli ],
+  [goerli],
   [
     alchemyProvider({ apiKey: alchemyApiKey }),
   ]
