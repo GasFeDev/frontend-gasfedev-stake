611f98f08429d7d84c2b9a56285ea170a4ea39ed
GasFeDev <gasfedev@gmail.com>
2023-07-06 19:08:16 -0300

    add window.env in wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,23 @@
+
 import { getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient } from 'wagmi';
 import { goerli } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
-const alchemyApiKey = process.env.REACT_APP_ALCHEMY_API_KEY!;
-const wallectConnectProjectId = process.env.REACT_APP_PROJECTID_WALLET_CONNECT!;
+interface CustomWindow extends Window {
+  env: {
+    REACT_APP_ALCHEMY_API_KEY: string;
+    REACT_APP_PROJECTID_WALLET_CONNECT: string;
+  };
+}
+
+declare const window: CustomWindow;
+
+
+const alchemyApiKey = window.env.REACT_APP_ALCHEMY_API_KEY!;
+const wallectConnectProjectId = window.env.REACT_APP_PROJECTID_WALLET_CONNECT;
+
+
 
 const { chains, provider, webSocketProvider } = configureChains(
   [goerli],
