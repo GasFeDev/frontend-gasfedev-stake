a2315ab35d79d1301f5a2fd4e897147efbeba93f
GasFeDev <gasfedev@gmail.com>
2023-07-06 17:24:24 -0300

    Update envaironment variable

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,6 +2,9 @@ import { getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient } from 'wagmi';
 import { goerli } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const alchemyApiKey = process.env.REACT_APP_ALCHEMY_API_KEY!;
 const wallectConnectProjectId = process.env.REACT_APP_PROJECTID_WALLET_CONNECT;
