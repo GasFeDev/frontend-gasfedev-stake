70d05c97b96cc5bab99288d2fe1cdac38e75501c
GasFeDev <gasfedev@gmail.com>
2023-07-06 23:03:43 -0300

    test  contracts.js

--- src/app/Utils/contracts.js
+++ src/app/Utils/contracts.js
@@ -1,14 +1,27 @@
+function getEnvironmentVariables() {
+  return {
+    REACT_APP_ALCHEMY_API_KEY: process.env.REACT_APP_ALCHEMY_API_KEY,
+    REACT_APP_PROJECTID_WALLET_CONNECT:
+      process.env.REACT_APP_PROJECTID_WALLET_CONNECT,
+    REACT_APP_ISPO_CONTRACT_ETH: process.env.REACT_APP_ISPO_CONTRACT_ETH,
+    REACT_APP_STETH_CONTRACT: process.env.REACT_APP_STETH_CONTRACT,
+    REACT_APP_ISPO_CONTRACT_POLYGON:
+      process.env.REACT_APP_ISPO_CONTRACT_POLYGON,
+    REACT_APP_STMATIC_CONTRACT: process.env.REACT_APP_STMATIC_CONTRACT,
+  };
+}
+
 const Contracts = {
   ethereum: {
-    ispoContract: process.env.REACT_APP_ISPO_CONTRACT_ETH,
+    ispoContract: getEnvironmentVariables().REACT_APP_ISPO_CONTRACT_ETH,
     ispoABI: require("../ABIs/DegaISPO.json"),
-    stContract: process.env.REACT_APP_STETH_CONTRACT,
+    stContract: getEnvironmentVariables().REACT_APP_STETH_CONTRACT,
     stABI: require("../ABIs/stETH.json"),
   },
   polygon: {
-    ispoContract: process.env.REACT_APP_ISPO_CONTRACT_POLYGON,
+    ispoContract: getEnvironmentVariables().REACT_APP_ISPO_CONTRACT_POLYGON,
     ispoABI: require("../ABIs/DegaISPOPolygon.json"),
-    stContract: process.env.REACT_APP_STMATIC_CONTRACT,
+    stContract: getEnvironmentVariables().REACT_APP_STMATIC_CONTRACT,
     stABI: require("../ABIs/stMATIC.json"),
   },
 };
